cd29036ea66fc80e55ab533810dec37062481189
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-09-08 13:45:56 +0200

    Set up Next.js project with Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-5xl text-center p-8 bg-indigo-400 text-white">
+        Hello, Developers
+      </h1>
+    </>
+  );
+}
