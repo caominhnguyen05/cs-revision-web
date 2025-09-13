3cf29e3b4924f0328c2c3496be5eb27268fa2dcb
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-09-13 17:39:46 +0200

    Add paper lists props to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import DashboardClientPage from "./profile-dashboard";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { getPastPaperLists } from "@/lib/actions/paper-actions";
 
 export default async function DashboardPage() {
   const session = await auth.api.getSession({
@@ -12,5 +13,9 @@ export default async function DashboardPage() {
     redirect("/auth");
   }
 
-  return <DashboardClientPage session={session} />;
+  const paperLists = await getPastPaperLists();
+
+  return (
+    <DashboardClientPage session={session} initialPaperLists={paperLists} />
+  );
 }
